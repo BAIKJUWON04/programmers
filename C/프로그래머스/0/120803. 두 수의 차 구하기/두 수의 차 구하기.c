e3f63e4a29e3d1344348bf301676d4dc5d31d87b
BAIKJUWON <baik040609@gmail.com>
2026-08-18 11:45:11 +0900

    [level 0] Title: 두 수의 차 구하기, Time: 0.02 ms, Memory: 5.32 MB -BaekjoonHub

---
+++ C/프로그래머스/0/120803. 두 수의 차 구하기/두 수의 차 구하기.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    answer = num1 - num2;
+    printf("%d - %d = %d", num1, num2, answer);
+    return answer;
+}
